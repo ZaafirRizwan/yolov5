3ad49e62ca6235018afa744133563baea26b7735
Zaafir Rizwan <108717727+ZaafirRizwan@users.noreply.github.com>
2023-06-11 17:46:16 +0500

    Update train.py
    Signed-off-by: Zaafir Rizwan <108717727+ZaafirRizwan@users.noreply.github.com>

--- train.py
+++ train.py
@@ -585,16 +585,16 @@ def main(opt, callbacks=Callbacks()):
 #         opt.cfg, opt.weights, opt.resume = '', ckpt, True  # reinstate
 #         LOGGER.info(f'Resuming training from {ckpt}')
 #     else:
-#         opt.data, opt.cfg, opt.hyp, opt.weights, opt.project = \
-#             check_file(opt.data), check_yaml(opt.cfg), check_yaml(opt.hyp), str(opt.weights), str(opt.project)  # checks
-#         assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
-#         if opt.evolve:
-#             if opt.project == str(ROOT / 'runs/train'):  # if default project name, rename to runs/evolve
-#                 opt.project = str(ROOT / 'runs/evolve')
-#             opt.exist_ok, opt.resume = opt.resume, False  # pass resume to exist_ok and disable resume
-#         if opt.name == 'cfg':
-#             opt.name = Path(opt.cfg).stem  # use model.yaml as name
-#         opt.save_dir = str(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))
+    opt.data, opt.cfg, opt.hyp, opt.weights, opt.project = \
+        check_file(opt.data), check_yaml(opt.cfg), check_yaml(opt.hyp), str(opt.weights), str(opt.project)  # checks
+    assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
+    if opt.evolve:
+        if opt.project == str(ROOT / 'runs/train'):  # if default project name, rename to runs/evolve
+            opt.project = str(ROOT / 'runs/evolve')
+        opt.exist_ok, opt.resume = opt.resume, False  # pass resume to exist_ok and disable resume
+    if opt.name == 'cfg':
+        opt.name = Path(opt.cfg).stem  # use model.yaml as name
+    opt.save_dir = str(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))
 
     # DDP mode
     device = select_device(opt.device, batch_size=opt.batch_size)
